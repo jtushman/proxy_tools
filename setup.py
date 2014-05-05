ce944701f82859e86fabf35e7f7daae13ec92254
Jonathan Tushman <jtushman@pipewave.com>
2014-05-05 12:35:18 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+required_modules = []
+packages = 'proxy'
+
+setup(name='proxy',
+      version='0.1.0',
+      description='Proxy Implementation',
+      url='http://github.com/jtushman/proxy',
+      author='Jonathan Tushman',
+      author_email='jonathan@zefr.com',
+      install_requires=required_modules,
+      license='MIT',
+      packages=packages,
+      zip_safe=False,
+      tests_require=['nose'],
+      test_suite='nose.collector',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          ]
+      )
